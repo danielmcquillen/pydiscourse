8a43d3a75e370da4159bcfe1400faf2f7660466f
Daniel McQuillen <daniel@mcquilleninteractive.com>
2020-10-28 07:59:06 +1100

    Fix api route for delete  user(s) from group

--- pydiscourse/client.py
+++ pydiscourse/client.py
@@ -1149,7 +1149,7 @@ class DiscourseClient(object):
 
         """
         return self._delete(
-            "/admin/groups/{0}/members.json".format(groupid), username=username
+            "/groups/{0}/members.json".format(groupid), username=username
         )
 
     def color_schemes(self, **kwargs):
